364a2dd85e239c77129c6d30034d91763283fe23
DariiaLeliukh <dariialelyukh@gmail.com>
2023-06-26 06:13:15 -0400

    Implement functionality to add new urls

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//convert the request body from a Buffer into string
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -19,11 +22,41 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  while (urlDatabase.hasOwnProperty(shortURL)) {
+    shortURL = generateRandomString();
+  }
+  urlDatabase[shortURL] = longURL;
+
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let result = '';
+  const length = 6;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
